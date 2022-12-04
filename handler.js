85078817011d6bfa9511de39e0813a0c25dd96bf
Joseph Carson <josephc89@fullsail.edu>
2022-12-04 16:00:10 -0500

    add hello/:name route

--- handler.js
+++ handler.js
@@ -14,6 +14,12 @@ app.get("/hello", (req, res, next) => {
   });
 });
 
+app.get("/hello/:name", (req, res, next) => {
+  return res.status(200).json({
+    message: `Hello ${req.params.name}!`,
+  });
+});
+
 app.use((req, res, next) => {
   return res.status(404).json({
     error: "Not Found",
